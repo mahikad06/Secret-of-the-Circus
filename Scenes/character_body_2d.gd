7b3ce55ba9d42b6891d24a12daef3a306060f597
cinnamahi <mahikad06@gmail.com>
2025-12-19 16:47:35 -0500

    Basic level 1

--- Scenes/character_body_2d.gd
+++ Scenes/character_body_2d.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
-const JUMP_VELOCITY = -550.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-@onready var animation = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Gravity.
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -15,12 +14,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		animation.play("moving")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.stop()
 
 	move_and_slide()
