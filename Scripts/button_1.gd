afc0697a14df344400f551d34f023a9a0d031414
Chi <142235792+Chiubani@users.noreply.github.com>
2025-12-27 01:40:15 -0500

    Modified scripts

--- Scripts/button_1.gd
+++ Scripts/button_1.gd
@@ -4,4 +4,4 @@ extends Area2D
 func _on_body_entered(body: CharacterBody2D):
 	print("end level")
 	timer.start()
-	get_tree().change_scene_to_file("res://Scenes/cutscene_2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
